654ad0fb65ab6e28efba6eab8b105749ad4225f4
Swethaa S <swethaasankarraman@gmail.com>
2023-05-15 08:10:34 +0530

    adding neural network files

---
+++ Neural Networks/feedforwardnetwork.py
@@ -0,0 +1,66 @@
+import numpy as np
+
+def sigmoid(x):
+    return 1/(1 + np.exp(-x))
+
+
+class Neuron:
+    def __init__(self, weight, bias):
+        self.weight = weight
+        self.bias = bias
+
+    def feedforward(self, inputs):
+        return sigmoid(np.dot(self.weight, inputs) + self.bias)
+    
+
+weights = np.array([0, 1]) # w1 = 0, w2 = 1
+bias = 4                   # b = 4
+n = Neuron(weights, bias)
+
+x = np.array([2, 3])       # x1 = 2, x2 = 3
+print(n.feedforward(x)) 
+
+
+# FF network
+
+class OurNeuralNetwork:
+  '''
+  A neural network with:
+    - 2 inputs
+    - a hidden layer with 2 neurons (h1, h2)
+    - an output layer with 1 neuron (o1)
+  Each neuron has the same weights and bias:
+    - w = [0, 1]
+    - b = 0
+  '''
+  def __init__(self):
+    weights = np.array([0, 1])
+    bias = 0
+
+    # The Neuron class here is from the previous section
+    self.h1 = Neuron(weights, bias)
+    self.h2 = Neuron(weights, bias)
+    self.o1 = Neuron(weights, bias)
+
+  def feedforward(self, x):
+    out_h1 = self.h1.feedforward(x)
+    out_h2 = self.h2.feedforward(x)
+
+    # The inputs for o1 are the outputs from h1 and h2
+    out_o1 = self.o1.feedforward(np.array([out_h1, out_h2]))
+
+    return out_o1
+
+network = OurNeuralNetwork()
+x = np.array([2, 3])
+print(network.feedforward(x))
+
+
+# Calculate Mean Square loss
+
+def mse_loss(y_true, y_pred):
+   return ((y_true - y_pred)**2).mean()
+
+# SGD
+
+
